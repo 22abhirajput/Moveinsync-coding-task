49d0977619211f999c8fc8d698a70c320cfe55fc
Abhishek Anand <89356711+22abhirajput@users.noreply.github.com>
2022-10-30 09:53:49 +0530

    Update DatabaseSequence.java

--- DatabaseSequence.java
+++ DatabaseSequence.java
@@ -1,4 +1,4 @@
-package com.backendassignment.backendassignment.models;
+package com.MoveInsync.Moveinsync.models;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
